02ccfb3abf0c41b27019e627ae868f24171f0b1a
Momo <seckndanane16@gmail.com>
2018-03-27 03:13:32 +0100

    1ere modification du premier commit

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 <html>
     <head>
         <title>learnphp</title>
+        <style>
+           h1{
+               margin:auto;
+               text-align:center;
+           }
+           p{
+               color:red;
+               font-face:arial;
+           }
+        </style>
     </head>
     <body>
+        <h1>ajout h1</h1>
         <?php
             echo 'bonjour <br>';  
             echo 'salut';
         ?>
+        <br>
+        <p>je suis le meilleur programmeur sur cette terre</p>
     </body>
 </html>
